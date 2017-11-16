b4dd99e29d3d42ea944ce989108c052777bf2cc9
Andrew Ripley <andrewripley99@yahoo.com>
2017-11-16 09:53:31 -0700

    Block Breaker Game

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public GameObject paddle;
+
+    private bool playing = false;
+    private Vector3 paddleToBallVector;
+    private Rigidbody2D rigid;
+	void Start () {
+       paddleToBallVector = this.transform.position - paddle.transform.position;
+        rigid = this.GetComponent<Rigidbody2D>();
+        Debug.Log(rigid);
+	}
+	
+	void Update () {
+        if (!playing) {
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                rigid.velocity = new Vector2(10, 20);
+                playing = true;
+            }
+        }
+
+
+        
+
+    }
+}
---
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    public int hp;
+    public Sprite[] brickStates;
+    // Use this for initialization
+    private LevelManager levelManager;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        hp--;
+        
+        if(hp<= 0)
+        {
+            Destroy(this.gameObject);
+            LevelManager.brickCount--;
+            print(LevelManager.brickCount);
+            levelManager.CheckBrickCount();
+        }
+        else {
+            GetComponent<SpriteRenderer>().sprite = brickStates[hp - 1];
+        }
+       
+    }
+}
